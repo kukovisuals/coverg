061c48c687b4980a8b48f5cc908de5e44564b755
kukovisuals <clubhousenet@live.com>
2022-06-08 12:03:46 -0400

    app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import InsurancePolicyCard from './components/InsurancePolicyCard';
-
+import 'react-app-polyfill/ie11';
 // Not ideal to have it on top of the tree, Its something important to keep in mind
 const addMaxWidthMobile = 834
 
